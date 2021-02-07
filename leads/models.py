54d0ed239d43d32d6e86f8df5948bf745a2cdf60
agreatdev <geobres@gmail.com>
2021-02-08 01:09:43 +0200

    creating the detail view,the create view,passing the context,and use the pk key .Finally create forms.py with the simplest form model of django which is forms

--- leads/models.py
+++ leads/models.py
@@ -20,4 +20,5 @@ class Lead(models.Model):
 class Agent(models.Model):
     user       =models.OneToOneField(User,on_delete=models.CASCADE)
    
- 
+    def __str__(self):
+        return self.user.username  
